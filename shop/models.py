3fab893f9fbe7e9f73ca758327a5ae78b3811f77
mohsenamoon1160417237 <60034673+mohsenamoon1160417237@users.noreply.github.com>
2021-02-27 13:03:22 +0330

    Update models.py

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from .slugify import slugify
+from .slugify import slugify  # For persian slugs
 from django.urls import reverse
 '''from whoosh.fields import Schema , TEXT
 from whoosh.analysis import StemmingAnalyzer
